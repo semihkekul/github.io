774d4ecb589874a57d3adb1d3040aa89d6caa087
Semih Kekul <semihkekul@gmail.com>
2020-09-12 19:51:55 +0200

    from ceng

---
+++ assets/Tilesheet/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="16" tileheight="32" tilecount="5376" columns="168">
+ <image source="tiles.png" trans="808080" width="2688" height="1025"/>
+</tileset>
